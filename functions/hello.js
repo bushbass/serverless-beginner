d718e5fec07fb61b5bbf5f329a6bba3f8b433d72
Alex Nielsen <alex.nielsen@gmail.com>
2021-06-12 18:19:39 -0400

    move axios out of own function

--- functions/hello.js
+++ functions/hello.js
@@ -2,13 +2,11 @@
 exports.handler = async event => {
   const axios = require('../node_modules/axios')
     const swapiName = ''
-     function getSwapi(){
-        axios.get('https://swapi.dev/api/people/1')
+    
+    const subject = event.queryStringParameters.name || 'World'
+    axios.get('https://swapi.dev/api/people/1')
         .then(res=>res.json())
         .then(data=>console.log(data))
-    }
-    const subject = event.queryStringParameters.name || 'World'
-    getSwapi()
     return {
       statusCode: 200,
       body: `Hello ${swapiName}!`
