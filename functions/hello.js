8896b809135f738d4c8b960b0831e31c04c93213
Alex Nielsen <alex.nielsen@gmail.com>
2021-06-12 18:31:29 -0400

    move all of get rquest

--- functions/hello.js
+++ functions/hello.js
@@ -2,12 +2,15 @@
 exports.handler = async event => {
   const axios = require('../node_modules/axios')
     const swapiName = {}
-    
-    const subject = event.queryStringParameters.name || 'World'
     axios.get('https://swapi.dev/api/people/1')
         .then(res=>res.json())
-        .then(data=>{swapiName=data;console.log('inside axios');})
+        .then(data=>{
+          swapiName=data;
+          console.log('inside axios');
+        })
         console.log(swapiName);
+    
+    const subject = event.queryStringParameters.name || 'World'
     return {
       statusCode: 200,
       body: `Hello ${swapiName.name}!`
