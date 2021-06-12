cf138de4466ec111c5b707fa7c896903d4c534ea
Alex Nielsen <alex.nielsen@gmail.com>
2021-06-12 16:15:32 -0400

    change to swapi

--- functions/hello.js
+++ functions/hello.js
@@ -1,7 +1,11 @@
 exports.handler = async event => {
     const subject = event.queryStringParameters.name || 'World'
+    const swapiName = ''
+    fetch('https://swapi.dev/api/people/1')
+        .then(res=>res.json())
+        .then(json=>swapiName=json.name)
     return {
       statusCode: 200,
-      body: `Hello ${subject}!`
+      body: `Hello ${swapiName}!`
     }
   }
