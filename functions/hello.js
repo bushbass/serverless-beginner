2bbad107ed4d8adadea200eb410f419bf9366b8f
Alex Nielsen <alex.nielsen@gmail.com>
2021-06-12 18:33:31 -0400

    added await

--- functions/hello.js
+++ functions/hello.js
@@ -2,13 +2,12 @@
 exports.handler = async event => {
   const axios = require('../node_modules/axios')
     const swapiName = {}
-    axios.get('https://swapi.dev/api/people/1')
+    await axios.get('https://swapi.dev/api/people/1')
         .then(res=>res.json())
         .then(data=>{
           swapiName=data;
           console.log('inside axios');
         })
-        console.log(swapiName);
     
     const subject = event.queryStringParameters.name || 'World'
     return {
