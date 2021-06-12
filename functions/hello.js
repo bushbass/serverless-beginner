851dcedb7c95380083c9d233a37d5e71d1c3888a
Alex Nielsen <alex.nielsen@gmail.com>
2021-06-12 17:42:23 -0400

    move axios require statement and get request

--- functions/hello.js
+++ functions/hello.js
@@ -2,10 +2,10 @@ const axios = require(axios)
 const swapiName = ''
 axios.get('https://swapi.dev/api/people/1')
     .then(res=>res.json())
+    .then(json=>swapiName=json.name)
 
 exports.handler = async event => {
     const subject = event.queryStringParameters.name || 'World'
-        .then(json=>swapiName=json.name)
     return {
       statusCode: 200,
       body: `Hello ${swapiName}!`
