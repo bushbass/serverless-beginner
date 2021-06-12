d6a0fac7f744fe0f3b20d6b36ed768e45b66e008
Alex Nielsen <alex.nielsen@gmail.com>
2021-06-12 17:58:58 -0400

    moved everything iside event

--- functions/hello.js
+++ functions/hello.js
@@ -1,12 +1,12 @@
-const axios = require(axios)
-const swapiName = ''
-function getSwapi(){
-    axios.get('https://swapi.dev/api/people/1')
-    .then(res=>res.json())
-    .then(json=>swapiName=json.name)
-}
 
 exports.handler = async event => {
+    const axios = require(axios)
+    const swapiName = ''
+    function getSwapi(){
+        axios.get('https://swapi.dev/api/people/1')
+        .then(res=>res.json())
+        .then(json=>swapiName=json.name)
+    }
     const subject = event.queryStringParameters.name || 'World'
     getSwapi()
     return {
