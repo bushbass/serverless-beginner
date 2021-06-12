46092586233fd47551d373a88fcdc8577695e6bb
Alex Nielsen <alex.nielsen@gmail.com>
2021-06-12 17:38:21 -0400

    add axios

--- functions/hello.js
+++ functions/hello.js
@@ -1,7 +1,9 @@
+const axios = require(axios)
+
 exports.handler = async event => {
     const subject = event.queryStringParameters.name || 'World'
     const swapiName = ''
-    fetch('https://swapi.dev/api/people/1')
+    axios.get('https://swapi.dev/api/people/1')
         .then(res=>res.json())
         .then(json=>swapiName=json.name)
     return {
