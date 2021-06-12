2dcc71cfa00c20c930035403020cd7b65ef8453b
Alex Nielsen <alex.nielsen@gmail.com>
2021-06-12 18:22:44 -0400

    .then save swapiName with data

--- functions/hello.js
+++ functions/hello.js
@@ -1,14 +1,14 @@
 
 exports.handler = async event => {
   const axios = require('../node_modules/axios')
-    const swapiName = ''
+    const swapiName = {}
     
     const subject = event.queryStringParameters.name || 'World'
     axios.get('https://swapi.dev/api/people/1')
         .then(res=>res.json())
-        .then(data=>console.log(data))
+        .then(data=>swapiName=data)
     return {
       statusCode: 200,
-      body: `Hello ${swapiName}!`
+      body: `Hello ${swapiName.name}!`
     }
   }
