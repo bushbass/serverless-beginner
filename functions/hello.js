965049251a3f96c0122ffddbb9627ecc9e8c6229
Alex Nielsen <alex.nielsen@gmail.com>
2021-06-12 18:14:24 -0400

    add consolelog to get

--- functions/hello.js
+++ functions/hello.js
@@ -2,10 +2,10 @@
 exports.handler = async event => {
   const axios = require('../node_modules/axios')
     const swapiName = ''
-    function getSwapi(){
+     function getSwapi(){
         axios.get('https://swapi.dev/api/people/1')
         .then(res=>res.json())
-        .then(json=>swapiName=json.name)
+        .then(json=>swapiName=json.name).then(console.log({swapiName}))
     }
     const subject = event.queryStringParameters.name || 'World'
     getSwapi()
